0b7a69d102f53321b0142de6b99e9f97343f7659
Bryce <bbeeson@ucsd.edu>
2023-05-24 16:13:01 -0700

    pa1

--- grade.sh
+++ grade.sh
@@ -9,7 +9,10 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 #checking if list examples exists
-if [ -e student-submission/ListExamples.java ]
+fileexist=`find student-submission -name ListExamples.java`
+echo $fileexist
+
+if [[ "$fileexist" =~ "ListExamples.java" ]];
 then    
     echo 'Exists'
 else
@@ -18,10 +21,11 @@ else
 fi
 
 #copying the info from submission to grading area
-cp -v student-submission/* grading-area/
+mv $fileexist grading-area/
 
 #compiling the submitted stuff from student
-javac grading-area/*.java
+javac $(find grading-area/ -name "*.java")
+
 
 #checking if successfully compiled
 if [ $? -eq 0 ]
@@ -31,6 +35,7 @@ else
     echo 'Failed to Compile Student Submission'
     exit 1
 fi
+
 
 cp *.java grading-area/
 cp lib/* grading-area/
@@ -59,7 +64,6 @@ fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
-#temp push
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
